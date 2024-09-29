28358a905213197ae58ee32c5b9eab9081b40c24
Nabendu Manna <mannanabendu2000@gmail.com>
2024-09-29 12:12:17 +0530

    user model

---
+++ src/model/User.model.ts
@@ -0,0 +1,45 @@
+import { accountsConnection } from "@/lib/dbConnect";
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+}
+
+const UserSchema: Schema<User> = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please use a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    } ,
+    verifyCode: {
+        type: String,
+        required: [true, 'Verify Code is required'],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, 'Verify Code Expiry is required'],
+    } 
+});
+
+const userModel = accountsConnection.model('User', UserSchema);
